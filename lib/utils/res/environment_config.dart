6a9288cfb9782e5c0808e5d998528e49600bd25f
neerajkunistal <neerajk@unistal.com>
2025-02-24 17:58:58 +0530

    purvabharti code modification

--- lib/utils/res/environment_config.dart
+++ lib/utils/res/environment_config.dart
@@ -28,7 +28,7 @@ class EnvironmentConfig extends InheritedWidget {
       case EnvironmentFlavours.developmentPurvaBharti:
         return "https://nmpplstations1.smartgasnet.com/";
       case EnvironmentFlavours.productionPurvaBharti:
-        return "https://pbgpl.smartgasnet.com/";
+        return "https://nmpplstations1.smartgasnet.com/";
       case EnvironmentFlavours.developmentIgl:
         return "https://igl.smartgasnet.com/";
       case EnvironmentFlavours.productionIgl:
