66ea9d2d514589f2d8d30fed2f58e015216aec8d
neerajkunistal <neerajk@unistal.com>
2025-03-04 11:01:06 +0530

    PBGPl Steel

--- lib/utils/res/environment_config.dart
+++ lib/utils/res/environment_config.dart
@@ -26,9 +26,9 @@ class EnvironmentConfig extends InheritedWidget {
       case EnvironmentFlavours.productionUnistal:
         return "http://unistal.smartgasnet.com/";
       case EnvironmentFlavours.developmentPurvaBharti:
-        return "https://nmpplstations1.smartgasnet.com/";
+        return "https://pbgpluat.smartgasnet.com/";
       case EnvironmentFlavours.productionPurvaBharti:
-        return "https://nmpplstations1.smartgasnet.com/";
+        return "https://pbgpl.smartgasnet.com//";
       case EnvironmentFlavours.developmentIgl:
         return "https://igl.smartgasnet.com/";
       case EnvironmentFlavours.productionIgl:
